8b6b726318d1bd20e6c6dc34542deb81b88fe5c8
sasban <azure.saswata@gmail.com>
2021-09-09 21:02:23 +0530

    Adding a Layout Wrapper Component.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
     const params = useParams();
@@ -6,6 +7,9 @@ const QuoteDetail = () => {
       <section>
         <h1>The Quote Detail Page</h1>
         <p>{params.quoteId}</p>
+        <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Comments />
+        </Route>
       </section>
     );
   };
