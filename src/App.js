69246d9aa8066201dcb6ccb9ef17067c14c67276
sasban <azure.saswata@gmail.com>
2021-09-09 20:36:08 +0530

    Brand New Quotes App.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-
 function App() {
   return (
-    <>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </>
+    <div>
+
+    </div>
   );
 }
 
